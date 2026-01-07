f381fb15f6725702a9b3f4257b49077a16f8f4f4
syed25041996-afk <syed25041996@gmail.com>
2026-01-08 00:03:07 +0530

    added basic working code for - added env url

--- home-backup/src/environments/environment.ts
+++ home-backup/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://192.168.1.108:3000/api'
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'http://192.168.1.108:3000/api'
+  // apiUrl: 'http://localhost:3000/api'
 };
