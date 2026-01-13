2914728551a47f082488073c7205ad7202be7fd7
syed25041996-afk <syed25041996@gmail.com>
2026-01-13 16:41:32 +0530

    changed env url

--- home-backup/src/environments/environment.prod.ts
+++ home-backup/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://home.tail2b1f38.ts.net:3000/api',
+    apiUrl: 'http://home.tail2b1f38.ts.net:3000/api',
 };
--- home-backup/src/environments/environment.ts
+++ home-backup/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://home.tail2b1f38.ts.net:3000/api',
+  apiUrl: 'http://home.tail2b1f38.ts.net:3000/api',
 };
