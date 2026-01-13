cec9382afd0df2946762ceea30ea469907305163
syed25041996-afk <syed25041996@gmail.com>
2026-01-13 21:32:22 +0530

    optimized routes

--- home-backup/src/app/app.routes.ts
+++ home-backup/src/app/app.routes.ts
@@ -4,8 +4,29 @@ import { DashboardComponent } from './components/dashboard/dashboard';
 import { RegisterComponent } from './components/register/register';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    title: 'Login | My App',
+    loadComponent: () => import('./components/login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    title: 'Sign Up | My App',
+    loadComponent: () => import('./components/register/register').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    // canActivate: [authGuard], // Protect the route
+    loadComponent: () => import('./components/dashboard/dashboard').then(m => m.DashboardComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'login' // Wildcard route for 404s
+  }
 ];
