148dec6ad376cf9b1f7d934e455d4bb81a6752da
syed25041996-afk <syed25041996@gmail.com>
2026-01-14 15:41:14 +0530

    added tailscale routing path - v5

--- home-backup/src/environments/environment.ts
+++ home-backup/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   // Use the Tailscale URL with the specific port you set for the backend
   apiUrl: '/api'
+  // apiUrl: 'https://home.tail2b1f38.ts.net:3000/api/'
 };
